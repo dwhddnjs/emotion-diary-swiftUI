3b5860f60995b97aed56faf6aec0193ef2f1c226
dwhddnjs <per1215@naver.com>
2025-06-16 06:12:47 +0900

    Initial Commit

---
+++ emotion-diary/emotion_diaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  emotion_diaryApp.swift
+//  emotion-diary
+//
+//  Created by 이종원 on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct emotion_diaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
